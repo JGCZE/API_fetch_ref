37e7167cde7f888c86861b2c1a39b9a5669f11e9
Jan Gross <jan.gross.cze@gmail.com>
2024-01-21 15:48:21 +0100

    sorting by last name

--- src/components/OneSpecificColabs.tsx
+++ src/components/OneSpecificColabs.tsx
@@ -1,7 +1,17 @@
 import style from "./OneSpecificColabs.module.css";
 
 const OneSpecificColabs = ({collab}: any) => {
+  //console.log(collab);
   
+  const sortAndGroupData = (data) => {    
+    data.sort((a,b) => a.lastName.localeCompare(b.lastName))
+
+    return data
+  }
+
+
+  sortAndGroupData(collab.collab)
+
   return (
     <div className={style.wrapper}>
       {collab.collab.map((item) => {
